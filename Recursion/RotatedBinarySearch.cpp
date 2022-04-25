b7456030cf2e1c69c2235f7adb3a87cdfd461362
VRaj361 <vrajnpatel16@gmail.com>
2022-04-25 18:37:57 +0530

    Recursion Day-1

---
+++ Recursion/RotatedBinarySearch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int search(int *arr,int tar,int s,int e){
+    if(s>e){
+        return 01;
+    }
+    int mid=s+(e-s)/2;
+    if(arr[mid]==tar){
+        return mid;
+    }
+    if(arr[s]<arr[mid]){
+        if(tar>=arr[s]&&tar<=arr[mid]){
+            return search(arr,tar,s,mid-1);
+        }else{
+            return search(arr,tar,mid+1,e);
+        }
+    }
+    if(arr[mid]>=tar&&arr[e]<=tar){
+        return search(arr,tar,mid+1,e);
+    }
+}
+int main(){
+    int arr[]={5,6,7,8,9,1,2,3};
+    cout<<search(arr,8,0,8);
+}
--- Recursion/linearsearch.cpp
+++ Recursion/linearsearch.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 void print(int arr[],int n){
     cout<<"printing the array"<<endl;
@@ -18,9 +19,26 @@ int linearsearch(int arr[],int n,int k){
     // cout<<remain<<endl;
     return remain;
 }
+static vector<int> v;
+void linearsearchall(int *arr,int n,int k,int s){
+    
+    if(s==n){
+        return ;
+    }
+    if(arr[s]==k){
+        v.push_back(s);
+    }
+    linearsearchall(arr,n,k,s+1);
+}
 int main(void)
 {
-    int arr[]={2,5,1,66,55,6};
+    int arr[]={2,5,1,6,55,6};
     linearsearch(arr,6,1);
+    linearsearchall(arr,6,6,0);
+    cout<<"[";
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<",";
+    }
+    cout<<"]";
     return 0;
 }
