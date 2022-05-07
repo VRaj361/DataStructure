4b47bd7e38b9da87269911b2d51a429b4c4d1274
VRaj361 <vrajnpatel16@gmail.com>
2022-05-07 23:24:19 +0530

    Linklist Day-4

---
+++ Linklist/MergeTwoSortedLinklist.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+template <typename T>
+    class Node {
+    public:
+    T data;
+    Node* next;
+
+    Node(T data) {
+        next = NULL;
+        this->data = data;
+    }
+
+    ~Node() {
+        if (next != NULL) {
+            delete next;
+        }
+    }
+};
+Node<int>* solve(Node<int>* first, Node<int>* second){
+	if(first->next==NULL){
+		first->next=second;
+		return first;
+	}
+	Node<int> *curr1=first;
+	Node<int> *next1=curr1->next;
+	Node<int> *curr2=second;
+	Node<int> *next2=curr2->next;
+	while(next1!=NULL&&curr2!=NULL){
+		if(( curr2->data>=curr1->data)&&(curr2->data<=next1->data)){
+			//node adition 
+			next2=curr2->next;
+			curr1->next=curr2;
+			curr2->next=next1;
+			
+			curr1=curr2;
+			curr2=next2;
+		}else{
+			curr1=next1;
+			next1=next1->next;
+			if(next1==NULL){
+				curr1->next=curr2;
+				return first;
+			}
+		}
+	}
+	return first;
+}
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    // Write your code here.
+	if(first==NULL){
+		return second;
+	}
+	if(second==NULL){
+		return first;
+	}
+
+	if(first->data<=second->data){
+		return solve(first,second);
+	}
+	else{
+		return solve(second,first);
+	}
+	return first;
+}
+void print(Node<int> *&head)
+{
+    Node<int> *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main(){
+    Node<int> *head = new Node<int>(1);
+    Node<int> *head1 = new Node<int>(4);
+    Node<int> *head2 = new Node<int>(5);
+    head->next = head1;
+    head1->next = head2;
+    
+    Node<int> *head4 = new Node<int>(2);
+    Node<int> *head5 = new Node<int>(3);
+    Node<int> *head6 = new Node<int>(5);
+    head4->next = head5;
+    head5->next = head6;
+
+    head=sortTwoLists(head,head4);
+    print(head);
+    
+}
---
+++ Linklist/Sort012.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *sortList(Node *head)
+{
+    // Write your code here.
+    int cz = 0, co = 0, ct = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == 0)
+        {
+            cz++;
+        }
+        else if (temp->data == 1)
+        {
+            co++;
+        }
+        else
+        {
+            ct++;
+        }
+        temp = temp->next;
+    }
+    temp = head;
+    while (temp != NULL)
+    {
+        if (cz != 0)
+        {
+            temp->data = 0;
+            cz--;
+        }
+        else if (co != 0)
+        {
+            temp->data = 1;
+            co--;
+        }
+        else if(ct!=0)
+        {
+            temp->data = 2;
+            ct--;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+void insertTail(Node *&tail,Node *curr){
+	tail->next=curr;
+	tail=curr;
+}
+Node* sortList1(Node *head)
+{
+    // Write your code here.
+	Node *zHead=new Node(-1);
+	Node *zTail=zHead;
+	Node *oHead=new Node(-1);
+	Node *oTail=oHead;
+	Node *tHead=new Node(-1);
+	Node *tTail=tHead;
+	
+	Node *temp=head;
+	while(temp!=NULL){
+		if(temp->data==0){
+			insertTail(zTail,temp);
+		}else if (temp->data==1){
+			insertTail(oTail,temp);
+		}else if(temp->data==2){
+			insertTail(tTail,temp);
+		}
+		temp=temp->next;
+	}
+	//merging 3 lists
+	if(oHead->next!=NULL){
+		zTail->next=oHead->next;
+	}
+	else{
+		zTail->next=tHead->next;
+	}
+	
+	oTail->next=tHead->next;
+	tTail->next=NULL;
+	
+	//merge done 
+	
+	head=zHead->next;
+	//delete useless nodes
+	delete zHead;
+	delete oHead;
+	delete tHead;
+	
+	return head;
+}
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = new Node(1);
+    Node *head1 = new Node(0);
+    Node *head2 = new Node(2);
+    Node *head3 = new Node(1);
+    Node *head4 = new Node(2);
+    Node *head5 = new Node(1);
+    head->next = head1;
+    head1->next = head2;
+    head2->next = head3;
+    head3->next = head4;
+    head4->next = head5;
+    // head=sortList(head);
+    head=sortList1(head);
+    print(head);
+}
