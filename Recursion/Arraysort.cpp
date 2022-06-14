1044add775267ce4177641944bef959058c34663
Vraj <vrajnpatel16@gmail.com>
2022-06-14 21:48:31 +0530

    Add Recursion

---
+++ Recursion/Arraysort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void sortarr(int arr[],int s){
+    if(s==0||s==1){
+        cout<<"true"<<endl;
+        return;
+    }
+    if(arr[0]>arr[1]){
+        cout<<"false"<<endl;
+        return;
+    } 
+    
+    sortarr(arr+1,s-1); 
+
+
+}
+
+
+int main(void)
+{
+    int arr[]={1,2,3,4,5};
+    sortarr(arr,4);
+    return 0;
+}
+    
