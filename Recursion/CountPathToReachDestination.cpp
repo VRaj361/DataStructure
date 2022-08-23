0b146afb467376b7fb77c42ce85f53585639e3d3
Vraj <vrajnpatel16@gmail.com>
2022-08-23 10:51:16 +0530

    Simple  soultion for count path

--- Recursion/CountPathToReachDestination.cpp
+++ Recursion/CountPathToReachDestination.cpp
@@ -11,8 +11,16 @@ int path(int i,int j,int n,int m){
     int right=path(i,j+1,n,m);
     return down+right;
 }
+
+int paths(int n,int m){
+    if(n==1||m==1){
+        return 1;
+    }
+    return paths(n-1,m)+paths(n,m-1);
+}
 //n and m value of matrix and how many way to react the destination
 //you can only move right and down 
 int main(){
-    cout<<path(0,0,3,3);
+    cout<<path(0,0,1,2)<<endl;
+    cout<<paths(1,2)<<endl;
 }
