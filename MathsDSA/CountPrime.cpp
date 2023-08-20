8dc50929d9af92ce843497909676d1140d90f8e0
VRaj361 <vrajnpatel16@gmail.com>
2023-08-20 20:37:29 +0530

    Optimized version of Sieve of Eratosthenes

--- MathsDSA/CountPrime.cpp
+++ MathsDSA/CountPrime.cpp
@@ -2,28 +2,33 @@
 #include <vector>
 using namespace std;
 
-//204 count prime leetcode
-//Sieve of Erathosthenes
+// 204 count prime leetcode
+// Sieve of Erathosthenes
 int countPrimes(int n)
 {
     int count = 0;
     vector<bool> prime(n + 1, true);
     prime[0] = prime[1] = false;
+    for (int i = 2; i * i < n; i++)
+    {
+        if (prime[i])
+        {
+            for (int j = i * i; j < n; j = j + i)
+            {
+                prime[j] = false;
+            }
+        }
+    }
     for (int i = 2; i < n; i++)
     {
         if (prime[i])
         {
             count++;
-
-            for (int j = 2 * i; j < n; j = j + i)
-            {
-                prime[j] = false;
-            }
         }
     }
     return count;
 }
 int main()
 {
-    cout<<countPrimes(18)<<endl;
+    cout << countPrimes(18) << endl;
 }
